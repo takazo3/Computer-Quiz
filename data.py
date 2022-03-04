c3575245bec8fe11a5992fca970774b538bdc90e
takazo <info@re-zero-jp.com>
2022-03-04 13:43:55 +0900

    Example quiz

---
+++ data.py
@@ -0,0 +1,65 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "medium",
+        "question": "The common software-programming acronym &quot;I18N&quot; comes from"
+                    " the term &quot;Interlocalization&quot;.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {"category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+     "question": "The first computer bug was formed by faulty wires.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]
+     },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "FLAC stands for &quot;Free Lossless Audio Condenser&quot;&#039;",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "&quot;Windows NT&quot; is a monolithic kernel.", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "The open source program Redis is a relational database server.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "Early RAM was directly seated onto the motherboard and could not be easily removed.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "The last Windows operating system to be based on the Windows 9x kernel was Windows 98.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "It&#039;s not possible to format a write-protected DVD-R Hard Disk.", "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "Android versions are named in alphabetical order.",
+        "correct_answer": "True", "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "Linus Sebastian is the creator of the Linux kernel, "
+                    "which went on to be used in Linux, Android, and Chrome OS.",
+        "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers", "type": "boolean", "difficulty": "medium",
+        "question": "A Boolean value of &quot;0&quot; represents which of these words?", "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
